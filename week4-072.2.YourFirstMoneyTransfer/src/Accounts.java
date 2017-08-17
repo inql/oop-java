185f5425dee2718a3de8b2d638b704e2cf21b0ca
Dawid Bińkuś <dbinkus96@gmail.com>
2017-08-17 23:36:57 +0200

    week 04 - exercise 072.2

---
+++ week4-072.2.YourFirstMoneyTransfer/src/Accounts.java
@@ -0,0 +1,14 @@
+
+public class Accounts {
+
+    public static void main(String[] args) {
+        // Code in Account.Java should not be touched!
+        // write your code here
+        Account mattsAccount = new Account ("Matt's account", 1000.00);
+        Account myAccount = new Account ("My account", 0.00);
+        mattsAccount.withdrawal(100.00);
+        myAccount.deposit(100.00);
+        System.out.print(mattsAccount+ "\n"+ myAccount);
+    }
+
+}
