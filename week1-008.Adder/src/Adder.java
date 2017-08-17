4a4280f29da57990f08515f15e29a2f946ec6db1
Dawid Bińkuś <dbinkus96@gmail.com>
2017-08-17 16:12:55 +0200

    week 01 - exercise 008

---
+++ week1-008.Adder/src/Adder.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+public class Adder {
+
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+
+        // Implement your program here. Remember to ask the input from user
+
+        System.out.print("Type a number: ");
+        int number = Integer.parseInt(reader.nextLine());
+        System.out.print("Type another number: ");
+        int anotherNumber = Integer.parseInt(reader.nextLine());
+        System.out.println();
+        System.out.println("Sum of the numbers: "+ (number + anotherNumber));
+    }
+}
