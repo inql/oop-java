bb933c594ad3048a2dc16409155464024e9abf00
Dawid Bińkuś <dbinkus96@gmail.com>
2017-08-17 16:39:58 +0200

    week 03 - exercise 067

---
+++ week3-067.TheVariance/src/Variance.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+
+public class Variance {
+    // Copy here sum from exercise 63 
+    public static int sum(ArrayList<Integer> list) {
+        int sum = 0;
+        for(int item : list)
+        {
+            sum+=item;
+        }
+        return sum;
+    }
+    
+    // Copy here average from exercise 64 
+    public static double average(ArrayList<Integer> list) {
+        return (double)sum(list)/list.size();
+    }
+
+    public static double variance(ArrayList<Integer> list) {
+        double variance = 0;
+        double average = average(list);
+        for(int i=0; i<list.size(); i++)
+        {
+            variance += Math.pow(list.get(i)-average,2);
+        }
+        variance/=list.size()-1;
+        return variance;
+    }
+    
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        list.add(3);
+        list.add(2);
+        list.add(7);
+        list.add(2);
+        
+        System.out.println("The variance is: " + variance(list));
+    }
+
+}
